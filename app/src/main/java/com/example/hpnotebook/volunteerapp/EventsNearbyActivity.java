30d3703940d7e668bd0f6898e7337aeb1c46a796
Nauman Ahmad <tnsd995@gmail.com>
2019-07-27 23:32:35 +0500

    made changes

---
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/EventsNearbyActivity.java
@@ -0,0 +1,19 @@
+package com.example.hpnotebook.volunteerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class EventsNearbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_nearby);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Events Nearby");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
---
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/Fragments/UserProfileFragment.java
@@ -0,0 +1,37 @@
+package com.example.hpnotebook.volunteerapp.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hpnotebook.volunteerapp.R;
+
+import java.util.Objects;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UserProfileFragment extends Fragment {
+
+
+    public UserProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+    }
+
+
+
+}
