bc0ea64055b2dbba446cb09290b8ed57218be2a8
Nauman Ahmad <tnsd995@gmail.com>
2019-09-01 18:26:59 +0500

    made changes

--- app/src/main/java/com/example/hpnotebook/volunteerapp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/Activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,16 +13,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.hpnotebook.volunteerapp.ModelClasses.User;
 import com.example.hpnotebook.volunteerapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText login_contact, login_pass;
+    EditText login_email, login_pass;
     Button login_btn;
     TextView tv_signup;
     private boolean fieldCheck;
+    private int LOGIN = 1;
+    ProgressDialog progressDialog;
+    FirebaseAuth auth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference userRef;
+    FirebaseUser user, firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,9 @@ public class LoginActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("Log in");
 
         init();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
@@ -51,20 +69,45 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void init() {
-        login_contact = findViewById(R.id.login_contact);
-        login_pass = findViewById(R.id.login_password);
-        login_btn = findViewById(R.id.login_btn);
-        tv_signup = findViewById(R.id.tv_signup);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //check if user is null
+        if (firebaseUser != null){
+            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+            finish();
+        }
+    }
+
+    private void authUser(String email, String pass) {
+
+        progressDialog.show();
+
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressDialog.dismiss();
+
+                if (task.isSuccessful()) {
+                    user = auth.getCurrentUser();
+                    startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                }
+            }
+        });
     }
 
     public void login() {
 
-        String email = login_contact.getText().toString();
+        String email = login_email.getText().toString();
         String pass = login_pass.getText().toString();
 
         if (email.isEmpty()) {
-            login_contact.setError("This field is empty");
+            login_email.setError("This field is empty");
             fieldCheck = true;
         }
         if (pass.isEmpty()) {
@@ -76,8 +119,19 @@ public class LoginActivity extends AppCompatActivity {
             fieldCheck = true;
         }
         if (!fieldCheck) {
-            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+            authUser(email, pass);
             finish();
         }
     }
+
+    private void init() {
+        login_email = findViewById(R.id.login_email);
+        login_pass = findViewById(R.id.login_password);
+        login_btn = findViewById(R.id.login_btn);
+        tv_signup = findViewById(R.id.tv_signup);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        userRef = firebaseDatabase.getReference("users");
+    }
+
 }
