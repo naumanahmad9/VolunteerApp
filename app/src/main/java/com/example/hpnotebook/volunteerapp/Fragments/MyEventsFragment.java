1d6359bafd1b0f6f914fc770ac2b59f1d3010fb2
Nauman Ahmad <tnsd995@gmail.com>
2019-09-06 02:52:38 +0500

    made changes

--- app/src/main/java/com/example/hpnotebook/volunteerapp/Fragments/MyEventsFragment.java
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/Fragments/MyEventsFragment.java
@@ -1,6 +1,5 @@
 package com.example.hpnotebook.volunteerapp.Fragments;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
--- app/src/main/java/com/example/hpnotebook/volunteerapp/SignupFragmentAdapter.java
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/SignupFragmentAdapter.java
@@ -1,4 +1,41 @@
 package com.example.hpnotebook.volunteerapp;
 
-public class SignupFragmentAdapter {
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+public class SignupFragmentAdapter extends FragmentStatePagerAdapter {
+
+    private ArrayList<Fragment> fragments;
+
+    public SignupFragmentAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Volunteer";
+            case 1:
+                return "Organization";
+            default:
+                return "";
+        }
+    }
 }
