24315f0b91ad25897e51d5a441a5c608292249f3
Nauman Ahmad <tnsd995@gmail.com>
2019-09-07 02:27:07 +0500

    made changes

--- app/src/main/java/com/example/hpnotebook/volunteerapp/Activities/PermissionActivity.java
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/Activities/PermissionActivity.java
@@ -1,15 +1,87 @@
 package com.example.hpnotebook.volunteerapp.Activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.hpnotebook.volunteerapp.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class PermissionActivity extends AppCompatActivity {
+
+    private Button button_allow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
+
+        button_allow = findViewById(R.id.button_allow);
+
+        if (ContextCompat.checkSelfPermission(PermissionActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            startActivity(new Intent(PermissionActivity.this, DashboardActivity.class));
+
+            finish();
+            return;
+        }
+
+        button_allow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(PermissionActivity.this)
+                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+                                startActivity(new Intent(PermissionActivity.this, DashboardActivity.class));
+                                finish();
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+                                if (response.isPermanentlyDenied()) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(PermissionActivity.this);
+                                    builder.setTitle("Permision denied")
+                                            .setMessage("Permission to access the device is permanently denied. You need to go the settings to allow the permission.")
+                                            .setNegativeButton("cancel", null)
+                                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    Intent intent = new Intent();
+                                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                    intent.setData(Uri.fromParts("package", getPackageName(), null));
+
+                                                }
+                                            }).show();
+                                } else {
+                                    Toast.makeText(PermissionActivity.this, "Permission Denied.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+
     }
 }
