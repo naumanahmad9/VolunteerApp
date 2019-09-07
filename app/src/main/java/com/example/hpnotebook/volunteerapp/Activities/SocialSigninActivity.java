691faefbc72c6021cf5e0e31f0ef9d7e284ef5f2
Nauman Ahmad <tnsd995@gmail.com>
2019-09-08 04:06:49 +0500

    made changes

---
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/Activities/SocialSigninActivity.java
@@ -0,0 +1,358 @@
+package com.example.hpnotebook.volunteerapp.Activities;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.hpnotebook.volunteerapp.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class SocialSigninActivity extends AppCompatActivity {
+
+    private Button mGoogleSignInBtn, mFacebookSignInBtn;
+
+    static final int RC_SIGN_IN = 1;
+    static final String TAG = "1234";
+
+    GoogleApiClient mGoogleApiClient;
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+
+    CallbackManager callbackManager;
+
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_signin);
+
+        mGoogleSignInBtn = findViewById(R.id.log_in_google);
+        mFacebookSignInBtn = findViewById(R.id.log_in_facebook);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        //databaseReference = database.getReference().child("users");
+
+        callbackManager = CallbackManager.Factory.create();
+
+        createProgressDialog();
+        checkingLoginState();
+        googleLogin();
+        setListenerOnButtons();
+
+    }
+
+    private void checkInternetConnectivity() {
+        if(!isOnline()){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(false);
+            builder.setTitle("Connect to Internet");
+            builder.setCancelable(false);
+            builder.setTitle("Connect to Internet");
+            builder.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.show();
+        }
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo;
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected();
+        }
+        return false;
+
+    }
+
+    private void createProgressDialog() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Loading...");
+    }
+
+    private void showProgressDialog(){
+        progressDialog.show();
+    }
+
+    private void hideProgressDialog(){
+        progressDialog.dismiss();
+    }
+
+    private void checkingLoginState() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+
+                     startActivity(new Intent(SocialSigninActivity.this, UserProfileActivity.class));
+                    //checkProfileSetUp(firebaseAuth.getCurrentUser());
+                }
+            }
+        };
+    }
+
+    private void setListenerOnButtons() {
+        mGoogleSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        mFacebookSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                facebookLogin();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkInternetConnectivity();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    //for google sign in............................................................
+
+    private void googleLogin() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("get it from google services file")
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                        Toast.makeText(SocialSigninActivity.this, "Connection Error", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        finish();
+//    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        showProgressDialog();
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(Objects.requireNonNull(account));
+            } else {
+                hideProgressDialog();
+                // Google Sign In failed
+                Toast.makeText(SocialSigninActivity.this,"Unable to Sign In",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            hideProgressDialog();
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(SocialSigninActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            //After You got your data add this to clear the priviously selected mail
+                            mGoogleApiClient.clearDefaultAccountAndReconnect();
+
+
+                            startActivity(new Intent(SocialSigninActivity.this, UserProfileActivity.class));
+
+                            //checkProfileSetUp(user);
+
+                            //startActivity(new Intent(LoginActivity.this,UpdateProfileActivity.class));
+                        }
+                    }
+                });
+    }
+
+    //for facebook sign in...........................................................
+
+    private void facebookLogin() {
+
+        LoginManager.getInstance().logInWithReadPermissions(SocialSigninActivity.this, Arrays.asList("email", "public_profile"));
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                hideProgressDialog();
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                hideProgressDialog();
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(SocialSigninActivity.this, UserProfileActivity.class));
+                            //checkProfileSetUp(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SocialSigninActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //checkProfileSetUp(null);
+
+                            startActivity(new Intent(SocialSigninActivity.this, UserProfileActivity.class));
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI(Boolean isProfileSet) {
+        hideProgressDialog();
+        Intent i;
+        if(isProfileSet){
+            i = new Intent(SocialSigninActivity.this,DashboardActivity.class);
+        }
+        else {
+            i = new Intent(SocialSigninActivity.this, UserProfileActivity.class);
+        }
+//        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(i);
+        finish();
+    }
+
+    private void checkProfileSetUp(FirebaseUser currentUser) {
+
+        if(currentUser != null){
+
+            databaseReference.child(currentUser.getUid()).child("profileisset").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String value = dataSnapshot.getValue(String.class);
+                    updateUI(TextUtils.equals(value,"1"));
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    updateUI(false);
+                }
+            });
+        }
+    }
+}
