9cf84db7c8db3a78fe867502158671c48bbcf919
Nauman Ahmad <tnsd995@gmail.com>
2019-09-10 04:16:10 +0500

    made changes

---
+++ app/src/main/java/com/example/hpnotebook/volunteerapp/CommentAdapter.java
@@ -0,0 +1,154 @@
+package com.example.hpnotebook.volunteerapp;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.hpnotebook.volunteerapp.Activities.UserProfileActivity;
+import com.example.hpnotebook.volunteerapp.ModelClasses.Comment;
+import com.example.hpnotebook.volunteerapp.ModelClasses.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ImageViewHolder> {
+
+    private Context mContext;
+    private List<Comment> mComment;
+    private String eventid;
+
+    private FirebaseUser firebaseUser;
+
+    public CommentAdapter(Context context, List<Comment> comments, String eventid){
+        mContext = context;
+        mComment = comments;
+        this.eventid = eventid;
+    }
+
+    @NonNull
+    @Override
+    public CommentAdapter.ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.comment_item, parent, false);
+        return new CommentAdapter.ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentAdapter.ImageViewHolder holder, final int position) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        final Comment comment = mComment.get(position);
+
+        holder.comment.setText(comment.getComment());
+        getUserInfo(holder.image_profile, holder.username, comment.getPublisher());
+
+        holder.username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(mContext, UserProfileActivity.class);
+                intent.putExtra("publisherid", comment.getPublisher());
+                mContext.startActivity(intent);
+            }
+        });
+
+        holder.image_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, UserProfileActivity.class);
+                intent.putExtra("publisherid", comment.getPublisher());
+                mContext.startActivity(intent);
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if (comment.getPublisher().equals(firebaseUser.getUid())) {
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+                    alertDialog.setTitle("Do you want to delete?");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "No",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    FirebaseDatabase.getInstance().getReference("Comments")
+                                            .child(eventid).child(comment.getCommentid())
+                                            .removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                Toast.makeText(mContext, "Deleted!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mComment.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView image_profile;
+        public TextView username, comment;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+
+            image_profile = itemView.findViewById(R.id.image_profile);
+            username = itemView.findViewById(R.id.username);
+            comment = itemView.findViewById(R.id.comment);
+        }
+    }
+
+    private void getUserInfo(final ImageView imageView, final TextView username, String publisherid){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("Users").child(publisherid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                Glide.with(mContext).load(user.getImageURL()).into(imageView);
+                username.setText(user.getName());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
